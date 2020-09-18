7e9f32f85b8e05bf5c2d1e968481b1fd417af0c5
Fabrizio <fabrynet.bianco@gmail.com>
2020-09-18 17:43:51 +0200

    Sistemate immagini nel Content

--- laravel/resources/views/layouts/main-layout.blade.php
+++ laravel/resources/views/layouts/main-layout.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>La Molisana</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href={{ asset('css/app.css') }}>
   </head>
   <body>
     <div class="container">
--- laravel/resources/views/molisana.blade.php
+++ laravel/resources/views/molisana.blade.php
@@ -2,24 +2,37 @@
 
 @section('content')
   @isset($data)
+
     <h1>LE LUNGHE</h1>
-    @foreach ($data as $pasta)
-      @if ($pasta['tipo'] === 'lunga')
-        <img src="{{ $pasta['src'] }}" alt="">
-      @endif
-    @endforeach
+    <div class="products">
+      @foreach ($data as $pasta)
+        @if ($pasta['tipo'] === 'lunga')
+          <img src="{{ $pasta['src'] }}" alt="">
+        @endif
+      @endforeach
+    </div>
+    <div class="clearfix"></div>
+
     <h1>LE CORTE</h1>
-    @foreach ($data as $pasta)
-      @if ($pasta['tipo'] === 'corta')
-        <img src="{{ $pasta['src'] }}" alt="">
-      @endif
-    @endforeach
+    <div class="products">
+      @foreach ($data as $pasta)
+        @if ($pasta['tipo'] === 'corta')
+          <img src="{{ $pasta['src'] }}" alt="">
+        @endif
+      @endforeach
+    </div>
+    <div class="clearfix"></div>
+
     <h1>LE CORTISSIME</h1>
-    @foreach ($data as $pasta)
-      @if ($pasta['tipo'] === 'cortissima')
-        <img src="{{ $pasta['src'] }}" alt="">
-      @endif
-    @endforeach
+    <div class="products">
+      @foreach ($data as $pasta)
+        @if ($pasta['tipo'] === 'cortissima')
+          <img src="{{ $pasta['src'] }}" alt="">
+        @endif
+      @endforeach
+    </div>
+    <div class="clearfix"></div>
+
   @endisset
   @empty ($data)
     <h1>Non ci sono prodotti da visualizzare</h1>
