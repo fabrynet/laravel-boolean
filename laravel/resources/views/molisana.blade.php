8bc15e637df8e7d01aa72acd4f54124e6550797d
Fabrizio <fabrynet.bianco@gmail.com>
2020-09-18 18:02:09 +0200

    Didascalia sotto ogni prodotto con titolo, cottura e peso.

--- laravel/resources/views/molisana.blade.php
+++ laravel/resources/views/molisana.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.main-layout')
 
 @section('content')
+
   @isset($data)
 
     <h1>LE LUNGHE</h1>
     <div class="products">
       @foreach ($data as $pasta)
         @if ($pasta['tipo'] === 'lunga')
-          <img src="{{ $pasta['src'] }}" alt="">
+          @include('partials.product')
         @endif
       @endforeach
     </div>
@@ -17,7 +18,7 @@
     <div class="products">
       @foreach ($data as $pasta)
         @if ($pasta['tipo'] === 'corta')
-          <img src="{{ $pasta['src'] }}" alt="">
+          @include('partials.product')
         @endif
       @endforeach
     </div>
@@ -27,14 +28,16 @@
     <div class="products">
       @foreach ($data as $pasta)
         @if ($pasta['tipo'] === 'cortissima')
-          <img src="{{ $pasta['src'] }}" alt="">
+          @include('partials.product')
         @endif
       @endforeach
     </div>
     <div class="clearfix"></div>
 
   @endisset
+
   @empty ($data)
     <h1>Non ci sono prodotti da visualizzare</h1>
   @endempty
+
 @endsection
---
+++ laravel/resources/views/partials/product.blade.php
@@ -0,0 +1,16 @@
+<div class="product">
+  <img src="{{ $pasta['src'] }}" alt="">
+  <div class="title">
+    {{ $pasta['titolo'] }}
+  </div>
+  <div class="description">
+    <ul>
+      <li>
+        Cottura: {{ $pasta['cottura'] }}
+      </li>
+      <li>
+        Peso: {{ $pasta['peso'] }}
+      </li>
+    </ul>
+  </div>
+</div>
